666df3647a6da52beda6b134789986bee9c32b93
baqnngg <il8696546@gmail.com>
2024-09-02 14:09:56 +0900

    insertDB

---
+++ code/insertdb.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function main() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB'); 
+
+        const database = client.db('testdb');
+        const collection = database.collection('testcollection');
+
+        const newDocument = {name : 'kimjungsik', age : 18};
+        const insertResult = await collection.insertOne(newDocument);
+        console.log('Insertes document:', insertResult.insertedId);
+    } catch(error){
+        console.error('Error :',error);
+    } finally{
+         await client.close();
+    }
+};
+
+main();
