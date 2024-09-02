cd3fc930ccb450079c7ebe9f2b18ff465b16c1dc
baqnngg <il8696546@gmail.com>
2024-09-02 15:39:12 +0900

    DB

--- insertDB.js
+++ insertDB.js
@@ -11,6 +11,7 @@ async function main() {
         const database = client.db('testdb');
         const collection = database.collection('testcollection');
 
+        // DB에 데이터 집어넣기
         const newDocument = {name : 'kimjungsik', age : 18};
         const insertResult = await collection.insertOne(newDocument);
         console.log('Insertes document:', insertResult.insertedId);
